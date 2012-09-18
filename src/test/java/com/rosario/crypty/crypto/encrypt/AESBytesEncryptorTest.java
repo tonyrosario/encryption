a824ca0687ca0e52ba3d2d97ee8d99aeb8023755
Tony Rosario <tony.rosario@gmail.com>
2012-09-18 14:51:45 -0500

    added test for length of encrypted data

--- src/test/java/com/rosario/crypty/crypto/encrypt/AESBytesEncryptorTest.java
+++ src/test/java/com/rosario/crypty/crypto/encrypt/AESBytesEncryptorTest.java
@@ -1,6 +1,15 @@
 package com.rosario.crypty.crypto.encrypt;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,8 +29,17 @@ public class AESBytesEncryptorTest {
 	}
 	
 	@Test
-	public void encryptReturnsNull() {
-		assertNull(encryptor.encrypt(null));
+	public void encryptedDataHasLengthOf48Bytes() throws InvalidKeyException,
+			InvalidAlgorithmParameterException, IllegalBlockSizeException,
+			BadPaddingException, NoSuchAlgorithmException, InvalidKeySpecException {
+		String stringToEncrypt = "My super secret 1337 P@SSW0rDz";
+
+		byte[] result = encryptor.encrypt(stringToEncrypt.getBytes());
+
+		//System.out.println("before: " + stringToEncrypt);
+		//System.out.println("after : " + new String(result));
+
+		assertEquals(48, result.length); // 32 (data) + 16 (iv)
 	}
 	
 	@Test
