7055bfd114f9403663fcd55a793fd60bd356064f
Tony Rosario <tony.rosario@gmail.com>
2012-09-18 15:16:33 -0500

    organized imports

--- src/test/java/com/rosario/crypty/crypto/encrypt/AESBytesEncryptorTest.java
+++ src/test/java/com/rosario/crypty/crypto/encrypt/AESBytesEncryptorTest.java
@@ -1,7 +1,6 @@
 package com.rosario.crypty.crypto.encrypt;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
